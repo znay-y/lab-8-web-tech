e919b62130e8f54e60f97c9903eca1f82c508afc
Z PC <zaineirshad.07@gmail.com>
2026-04-07 07:56:38 +0100

    Complete exersize 4 files

---
+++ exersize4.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Topic 8 - Exercise 4</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link href="css/exercise4.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <?php
+
+    include("login-data.php");
+    $firstTime = true;
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $firstTime = false;
+        $email = $_POST['emailin'];
+        $password = $_POST['passwordin'];
+    }
+
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-8">
+                <div id="login">
+                    <div class="page-header">
+                        <h3>Login</h3>
+                    </div>
+                    <form role="form" method="POST" action="exersize4.php">
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="inputEmail3" placeholder="Email" name="emailin">
+                                <?php
+                                if ($firstTime == true) {
+                                } else {
+                                    if ($email == "") {
+                                        print("<span>&#215;</span>");
+                                    } else {
+                                        print("<span>&#10003;</span>");
+                                    }
+                                }
+
+                                ?>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Password</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="inputPassword3" placeholder="Password" name="passwordin">
+                                <?php
+                                if ($firstTime == true) {
+                                } else {
+                                    if ($password == "") {
+                                        print("<span>&#215;</span>");
+                                    } else {
+                                        print("<span>&#10003;</span>");
+                                    }
+                                }
+                                ?>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Module</label>
+                            <div class="col-sm-10">
+                                <select class="form-control">
+                                    <?php
+
+                                    for ($i = 401; $i < 407; $i++) {
+
+                                        print("<option>ECS$i</option>");
+                                    }
+
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-success">Submit</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-2">
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+ ================================================== -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ login-data.php
@@ -0,0 +1,5 @@
+<?php
+
+$email = "test";
+$password = "password";
+?>
